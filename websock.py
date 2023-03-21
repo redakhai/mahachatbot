4420dda5e8303841bd2c6d3415c661f3e0a1420a
redakhai <redastudies@gmail.com>
2023-03-21 22:30:41 +0000

    Update websock.py

--- websock.py
+++ websock.py
@@ -64,7 +64,7 @@ async def echo(websocket):
         await websocket.send(res)
 
 async def main():
-    async with websockets.serve(echo, "localhost", 8765):
+    async with websockets.serve(echo, '51.222.85.55' , 8765):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
